36fae72ea04778a3dd0a0699e0cba47856135b46
sergsagan <sergey.martynuk@gmail.com>
2017-06-23 15:07:25 +0300

    filter ready

--- src/js/script.js
+++ src/js/script.js
@@ -104,6 +104,31 @@ $(function() {
     });
 
 
+//filter
+
+    $('.filter li > a').on('click', function(event) {
+        event.preventDefault();
+
+        $(this).removeAttr('href');
+        var element = $(this).parent('li');
+        if (element.hasClass('open')) {
+            element.removeClass('open');
+            element.find('li').removeClass('open');
+            element.find('ul').slideUp();
+        } else {
+            element.addClass('open');
+            element.children('ul').slideDown();
+            element.siblings('li').children('ul').slideUp();
+            element.siblings('li').removeClass('open');
+            element.siblings('li').find('li').removeClass('open');
+            element.siblings('li').find('ul').slideUp();
+        }
+    });
+
+
+    $('.filter > ul > li.has-sub > a').append('<span class="icon icon-mini-arr-down"></span>');
+
+    $('.filter > ul > li > ul > li.has-sub > a').append('<span class="icon icon-mini-arr-down"></span>');
 
 
 
